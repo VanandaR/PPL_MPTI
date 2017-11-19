d9ef138320659f550dd90fa5664ae31f61e8efd1
Vananda Rahadika <vanandarahadika79@gmail.com>
2017-11-19 19:20:15 +0700

    fix validator in store and upload image

--- app/Http/Controllers/ternakController.php
+++ app/Http/Controllers/ternakController.php
@@ -8,7 +8,7 @@ use App\Http\Requests;
 
 use App\ternak;
 use App\user;
-
+use Validator;
 
 use Auth;
 
@@ -27,8 +27,8 @@ class ternakController extends Controller
         // }else{
         //     $data = Ide::where('id_user','=',Auth::user()->id)->get();
         // }
-        $data = ternak::all();
-        return view('peternak.tambahternak',compact('data'));
+      $data = ternak::all();
+      return view('peternak.tambahternak',compact('data'));
     }
 
     public function create()
@@ -44,79 +44,79 @@ class ternakController extends Controller
      */
     public function store(Request $request)
     {
-        if ((($_FILES["file"]["type"] == "image/gif") || ($_FILES["file"]["type"] == "image/jpeg") || ($_FILES["file"]["type"] == "image/jpg") || ($_FILES["file"]["type"] == "image/pjpeg")) && ($_FILES["file"]["size"] < 20000000)){
-          if ($_FILES["file"]["error"] > 0){
-            return back();
-          }
-        }else{
-            return back();
-        }
-        $data = new ternak;
-        $data->id_user = Auth::user()->id;
-        $data->jenisHewan = $request->jenisHewan;
-        $data->harga=$request->harga;
-        $data->bobot=$request->bobot;
-        $data->umur=$request->umur;
-        $data->lokasi=$request->lokasi;
-        $data->deskripsi=$request->deskripsi;
-        $data->status='belum terjual';
-        // $data->save();
-        $dat = ternak::insertGetId($data->toArray());
-        // echo $dat;
-        if ((($_FILES["file"]["type"] == "image/gif") || ($_FILES["file"]["type"] == "image/jpeg") || ($_FILES["file"]["type"] == "image/jpg") || ($_FILES["file"]["type"] == "image/pjpeg")) && ($_FILES["file"]["size"] < 20000000)){
-          if ($_FILES["file"]["error"] > 0){
-            return back();
-          }else{
-              $imageName = 'ternak'.$dat.'.' . pathinfo($_FILES['file']['name'],PATHINFO_EXTENSION);
-              move_uploaded_file($_FILES["file"]["tmp_name"],
-                  $_SERVER['DOCUMENT_ROOT'].'/img/ternak/' . $imageName);
-          }
-        }else{
-            return back();
-        }
-        return redirect('peternak/home');
+     $validator = Validator::make(request()->all(), [
+      'harga'  => 'required',
+            'bobot'  => 'required',
+            'foto'=>'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+            'umur'=>'required'
+      ]);
+     if ($validator->fails()) {
+      return back()
+      ->withErrors($validator->errors());
+    }
+    else{
+      $data = new ternak;
 
+      if( $request->hasFile('foto')) {
+        $photoName = time().'.'.$request->foto->getClientOriginalExtension();
+        $request->foto->move(public_path('/img/ternak'), $photoName);
+        $data->foto=$photoName;
+      }
+      $data->id_user = Auth::user()->id;
+
+      $data->jenisHewan = $request->jenisHewan;
+      $data->harga=$request->harga;
+      $data->bobot=$request->bobot;
+      $data->umur=$request->umur;
+      $data->lokasi=$request->lokasi;
+      $data->deskripsi=$request->deskripsi;
+      $data->status='belum terjual';
+        // $data->save();
+      $dat = ternak::insertGetId($data->toArray());
+        // echo $dat;
+      return redirect('peternak/home');
     }
-public function miliksemua(){
-        $users=user::all();
-        $data = ternak::join('users','ternak.id_user','=','users.id')->get();
-        
-        return view('peternak.home',compact('data','users')); 
-    }
-    public function lihatsemua(){
-    
-        $data = ternak::all();
-        
-        return view('investor.home',compact('data')); 
-    }
-      public function sapiperah(){
-    
-        $data = ternak::where('jenisHewan','=','sapi perah dewasa')->get();
-        
-        return view('investor.home',compact('data')); 
-    }
-      public function sapibali(){
-    
-        $data = ternak::where('jenisHewan','=','sapi bali dewasa')->get();
-        
-        return view('investor.home',compact('data')); 
-    }
-        public function sapisimental(){
-    
-        $data = ternak::where('jenisHewan','=','sapi simental dewasa')->get();
-        
-        return view('investor.home',compact('data')); 
-    }
-      public function miliksendiri(){
-        $users=user::all();
-        $data = ternak::join('users','ternak.id_user','=','users.id')->where('users.id','=',Auth::user()->id)->get();
-        
-        return view('peternak.home',compact('data','users')); 
-    }
-    public function hitung3bulan(){
-        $users=user::all();
-        
-    }
+  }
+  public function miliksemua(){
+    $users=user::all();
+    $data = ternak::join('users','ternak.id_user','=','users.id')->get();
+
+    return view('peternak.home',compact('data','users')); 
+  }
+  public function lihatsemua(){
+
+    $data = ternak::all();
+
+    return view('investor.home',compact('data')); 
+  }
+  public function sapiperah(){
+
+    $data = ternak::where('jenisHewan','=','sapi perah dewasa')->get();
+
+    return view('investor.home',compact('data')); 
+  }
+  public function sapibali(){
+
+    $data = ternak::where('jenisHewan','=','sapi bali dewasa')->get();
+
+    return view('investor.home',compact('data')); 
+  }
+  public function sapisimental(){
+
+    $data = ternak::where('jenisHewan','=','sapi simental dewasa')->get();
+
+    return view('investor.home',compact('data')); 
+  }
+  public function miliksendiri(){
+    $users=user::all();
+    $data = ternak::join('users','ternak.id_user','=','users.id')->where('users.id','=',Auth::user()->id)->get();
+
+    return view('peternak.home',compact('data','users')); 
+  }
+  public function hitung3bulan(){
+    $users=user::all();
+
+  }
 
     /**
      * Display the specified resource.
@@ -162,4 +162,4 @@ public function miliksemua(){
     {
         //
     }
-}
+  }
--- resources/views/peternak/tambahternak.blade.php
+++ resources/views/peternak/tambahternak.blade.php
@@ -2,7 +2,7 @@
   @section('middle')
   <!-- Middle Column -->
   <div class="w3-col m7">
-    
+
     <div class="w3-row-padding">
       <div class="w3-col m12">
         <div class="w3-card w3-round w3-white">
@@ -12,35 +12,42 @@
               <h6 class="w3-opacity">Tambah Hewan</h6>
               <hr class="w3-clear">
               <label for="file"  style="width: 100px;">Gambar: </label>
-              <input type="file" name="file" id="file" class="w3-rest w3-border" accept=".jpg">
+              <input type="file" name="foto" id="file" class="w3-rest w3-border">
               <br>
+              @if ($errors->has('foto')) <p style="color: red">{{ $errors->first('foto') }}</p> @endif
+
               <label for="jenisHewan"  style="width: 100px;">Jenis Hewan : </label>
-             <select name="jenisHewan" id="jenisHewan" style="margin-top: 20px;" class="form-control">
-              <option value="sapi perah dewasa">Sapi Perah Dewasa</option>
-              <option value="sapi bali dewasa">Sapi Bali Dewasa</option>
-              <option value="sapi simental dewasa">Sapi Simental Dewasa</option>
+              <select name="jenisHewan" id="jenisHewan" style="margin-top: 20px;" class="form-control">
+                <option value="sapi perah dewasa">Sapi Perah Dewasa</option>
+                <option value="sapi bali dewasa">Sapi Bali Dewasa</option>
+                <option value="sapi simental dewasa">Sapi Simental Dewasa</option>
               </select>
               <br>
               <label for="harga"  style="width: 100px;">Harga : </label>
-              <input type="text" name="harga" id="harga" class="w3-rest w3-border" required>
-              <br>
-                <label for="bobot"  style="width: 100px;">Bobot (kg) : </label>
-              <input type="text" name="bobot" id="bobot" class="w3-rest w3-border" required>
-              <br>
+              <input type="text" name="harga" id="harga" class="w3-rest w3-border">
 
-               <label for="umur"  style="width: 100px;">Umur (bulan) : </label>
-              <input type="text" name="umur" id="umur" class="w3-rest w3-border" required>
               <br>
+              @if ($errors->has('harga')) <p style="color: red">{{ $errors->first('harga') }}</p> @endif
+              <label for="bobot"  style="width: 100px;">Bobot (kg) : </label>
+              <input type="text" name="bobot" id="bobot" class="w3-rest w3-border">
+              <br>
+              @if ($errors->has('bobot')) <p style="color: red">{{ $errors->first('bobot') }}</p> @endif
+
+              <label for="umur"  style="width: 100px;">Umur (bulan) : </label>
+              <input type="text" name="umur" id="umur" class="w3-rest w3-border">
+              <br>
+              @if ($errors->has('umur')) <p style="color: red">{{ $errors->first('umur') }}</p> @endif
+
               <label for="lokasi"  style="width: 100px;">Lokasi </label>
               <br>
 
-              <textarea  name="lokasi" contenteditable="true" class="w3-border w3-padding w3-col" required></textarea>
+              <textarea  name="lokasi" contenteditable="true" class="w3-border w3-padding w3-col"></textarea>
               <br>
               <label for="deskripsi"  style="width: 100px;">Deskripsi </label>
               <br>S
-              <textarea  name="deskripsi" contenteditable="true" class="w3-border w3-padding w3-col" required></textarea>
-               <br>
-             
+              <textarea  name="deskripsi" contenteditable="true" class="w3-border w3-padding w3-col"></textarea>
+              <br>
+
               <hr class="w3-clear">
               <button type="submit" class="w3-button w3-theme w3-col"><i class="fa fa-pencil"></i>Tambah Hewan </button> 
             </form>
@@ -48,8 +55,8 @@
         </div>
       </div>
     </div>
-   
-   <!--  -->
+
+    <!--  -->
     <!-- End Middle Column -->
   </div>
   @if(Auth::user()->status == 'Komunitas')
@@ -69,4 +76,4 @@
   @endsection
 
 
-F
+  F
